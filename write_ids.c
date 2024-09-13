f6b41e394a412b226ed7412dd3d6600515219caf
aqmarnina <ank5683@psu.edu>
2024-09-13 14:54:39 -0400

    final

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = "aqmarnina";
+    char STUDENT_ID[] = "973195396";
 
     return 0 ;
 }
